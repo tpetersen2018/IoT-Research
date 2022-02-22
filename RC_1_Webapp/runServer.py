6e69a0f1d2133027b17c413c642696b4cfe7d0ce
carlinux727 <78164307+carlinux727@users.noreply.github.com>
2022-02-22 16:05:24 -0500

    changed to wiringpi

--- RC_1_Webapp/runServer.py
+++ RC_1_Webapp/runServer.py
@@ -1,65 +1,74 @@
-#import RPi.GPIO as GPIO
+import wiringpi as GPIO
 from flask import Flask, render_template, Response, request, url_for
 import sys
+from signal import *
 import time
 
 app = Flask(__name__, static_url_path="", static_folder="templates")
 app.config.from_pyfile('config.py')
 
-in1, in2, ena, in3, in4, enb = (11, 12, 13, 15, 16, 18)
-pwm_freq = 100
-pwm_l = None
-pwm_r = None
-duty = 50
+# Define the pins
+ENA = 22
+ENB = 30
+IN1 = 26
+IN3 = 21
+
+# Termination signal handler - makes sure motor is turned off
+def clean(*args):
+    print('Cleaning up GPIO')
+    GPIO.digitalWrite(ENA, 0)
+    GPIO.digitalWrite(ENB, 0)
+    GPIO.digitalWrite(IN1, 0)
+    GPIO.digitalWrite(IN3, 0)
+    sys.exit(0)
+
+for sig in (SIGABRT, SIGILL, SIGINT, SIGSEGV, SIGTERM, SIGQUIT, SIGTSTP):
+    signal(sig, clean)
 
 
 def initialize_gpio():
-    global in1, in2, in3, in4, ena, enb, duty, pwm_l, pwm_r, pwm_freq
-    GPIO.setmode(GPIO.BOARD)	# use board pin numbers
-    gates = (in1, in2, in3, in4)
-    # Initialize the IN gates to be output low
-    GPIO.setup(gates, GPIO.OUT)
-    GPIO.output(gates, GPIO.LOW)
-    # Setup the PWM signals
-    GPIO.setup((ena, enb), GPIO.OUT)
-    pwm_l = GPIO.PWM(ena, pwm_freq)
-    pwm_r = GPIO.PWM(enb, pwm_freq)
-    pwm_l.start(duty)
-    pwm_r.start(duty)
+    # Call setup function
+    GPIO.wiringPiSetup()
+    # Initialize pins as outputs
+    print(ENA, ENB, IN1, IN3)
+    print('22, 30, 26, 21')
+    GPIO.pinMode(ENA, GPIO.OUTPUT)
+    GPIO.pinMode(ENB, GPIO.OUTPUT)
+    GPIO.pinMode(IN1, GPIO.OUTPUT)
+    GPIO.pinMode(IN3, GPIO.OUTPUT)
+
+# Turn on PWM
+def turn_on():
+    GPIO.digitalWrite(ENA, GPIO.HIGH)
+    GPIO.digitalWrite(ENB, GPIO.HIGH)
 
 # Begin defining directions
 def backward():
-    global in1, in2, in3, in4, ena, enb, duty, pwm_l, pwm_r, pwm_freq
-    # Set IN2 & IN4 low FIRST
-    GPIO.output((in2, in4), GPIO.LOW)
-    # Now set IN1 & IN3 high
-    GPIO.output((in1, in3), GPIO.HIGH)
+    # Set gates
+    GPIO.digitalWrite(IN1, GPIO.LOW)
+    GPIO.digitalWrite(IN3, GPIO.HIGH)
+    # set enable signals
+    turn_on()
 
 def forward():
-    global in1, in2, in3, in4, ena, enb, duty, pwm_l, pwm_r, pwm_freq
-    # Set IN1 & IN3 low
-    GPIO.output((in1, in3), GPIO.LOW)
-    # Now set IN2 & IN4 high
-    GPIO.output((in2, in4), GPIO.HIGH)
+    GPIO.digitalWrite(IN1, GPIO.HIGH)
+    GPIO.digitalWrite(IN3, GPIO.LOW)
+    turn_on()
 
 def left():
-    global in1, in2, in3, in4, ena, enb, duty, pwm_l, pwm_r, pwm_freq
-    # Set IN1 & IN4 low first
-    GPIO.output((in1, in4), GPIO.LOW)
-    # Now set IN2 & IN3 high
-    GPIO.output((in2, in3), GPIO.HIGH)
+    GPIO.digitalWrite(IN1, GPIO.HIGH)
+    GPIO.digitalWrite(IN3, GPIO.HIGH)
+    turn_on()
 
 def right():
-    global in1, in2, in3, in4, ena, enb, duty, pwm_l, pwm_r, pwm_freq
-    # SET IN2 & IN3 low first
-    GPIO.output((in2, in3), GPIO.LOW)
-    # Now set IN1 & IN4 high
-    GPIO.output((in1, in4), GPIO.HIGH)
+    GPIO.digitalWrite(IN1, GPIO.LOW)
+    GPIO.digitalWrite(IN3, GPIO.LOW)
+    turn_on()
 
 def stop():
-    global in1, in2, in3, in4, ena, enb, duty, pwm_l, pwm_r, pwm_freq
-    # Set all inputs to low
-    GPIO.output((in1, in2, in3, in4), GPIO.LOW)
+    # Turn off enable signals
+    GPIO.digitalWrite(ENA, GPIO.LOW)
+    GPIO.digitalWrite(ENB, GPIO.LOW)
     
 #-------------------------------------------------#
 # Objective 1 Replay
@@ -73,27 +82,27 @@ def move_input():
     content = request.json
     
     if content['zoom'] == "forward":
-#        forward()
+        forward()
         print("User requested forward movement...")
         return "Forward movement request successful!"
         
     elif content['zoom'] == "left":
-#        left()
+        left()
         print("User requested left movement...")
         return "Left movement request successful!"
         
     elif content['zoom'] == "right":
-#        right()
+        right()
         print("User requested right movement...")
         return "Right movement request successful!"
         
     elif content['zoom'] == "backward":
-#        backward()
+        backward()
         print("User requested backward movement...")
         return "Backward movement request successful!"
         
     elif content['zoom'] == "stop":
-#        stop()
+        stop()
         print("User requested to stop movement...")
         return "Stopping movement!"
     
@@ -113,6 +122,6 @@ def cookie():
 #-------------------------------------------------#
        
 if __name__ == '__main__':
-#    GPIO.cleanup()
-#    initialize_gpio()
+    initialize_gpio()
     app.run(debug=True, port=80, host='0.0.0.0')
+    clean()

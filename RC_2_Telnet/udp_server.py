9921d4c5ee2ed7f2c09ff7cc4fd86214c6add983
carlinux727 <78164307+carlinux727@users.noreply.github.com>
2022-02-22 16:03:36 -0500

    changed to wiringpi

--- RC_2_Telnet/udp_server.py
+++ RC_2_Telnet/udp_server.py
@@ -1,17 +1,35 @@
-import RPi.GPIO as GPIO
+import wiringpi as GPIO
 import socket, select, sys
-import keyboard
+from signal import *
+#import keyboard
 
-GPIO.cleanup()
+# Define pins
+ENA = 22
+ENB = 30
+IN1 = 26
+IN3 = 21
+
+# Termination signal handler - makes sure motor is turned off
+def clean(*args):
+	print('Cleaning up GPIO')
+	GPIO.softPwmWrite(ENA, 0)
+	GPIO.softPwmWrite(ENB, 0)
+	GPIO.digitalWrite(IN1, 0)
+	GPIO.digitalWrite(IN3, 0)
+	sys.exit(0)
+
+for sig in (SIGABRT, SIGILL, SIGINT, SIGSEGV, SIGTERM, SIGQUIT, SIGTSTP):
+	signal(sig, clean)
+
 
 class WifiCar:
-	# Constructor - Need a tuple with 6 GPIO pin numbers: (IN1, IN2, IN3, IN4, ENA, ENB)
+	# Constructor - Need a tuple with 4 GPIO pin numbers: (ENA, ENB, IN1, IN3)
 	def __init__(self, ports, freq=100):
 		self._epoll = select.epoll()
 		self.pwm_freq = freq
-		self.in1, self.in2, self.ena, self.in3, self.in4, self.enb = ports
-		print(f'IN1:{self.in1}, IN2:{self.in2}, ENA:{self.ena}')
-		print(f'IN3:{self.in3}, IN4:{self.in4}, ENB:{self.enb}')
+		self.ena, self.enb, self.in1, self.in3 = ports
+		print(f'IN1:{self.in1}, ENA:{self.ena}')
+		print(f'IN3:{self.in3}, ENB:{self.enb}')
 		self.client_socks = {}
 
 	def initialize_udpserver(self, port=31337):
@@ -25,47 +43,57 @@ class WifiCar:
 
 
 	def initialize_gpio(self, duty=50):
-		GPIO.setmode(GPIO.BOARD)	# use board pin numbers
-		gates = (self.in1, self.in2, self.in3, self.in4)
-		# Initialize the IN gates to be output low
-		GPIO.setup(gates, GPIO.OUT)
-		GPIO.output(gates, GPIO.LOW)
-		# Setup the PWM signals
-		GPIO.setup((self.ena, self.enb), GPIO.OUT)
-		self.pwm_l = GPIO.PWM(self.ena, self.pwm_freq)
-		self.pwm_r = GPIO.PWM(self.enb, self.pwm_freq)
-		self.duty = duty
-		self.pwm_l.start(duty)
-		self.pwm_r.start(duty)
+		# call setup function
+		GPIO.wiringPiSetup()
+		# Initialize pins as outputs
+		GPIO.pinMode(self.ena, GPIO.OUTPUT)
+		GPIO.pinMode(self.enb, GPIO.OUTPUT)
+		GPIO.pinMode(self.in1, GPIO.OUTPUT)
+		GPIO.pinMode(self.in3, GPIO.OUTPUT)
+		# Create PWM signals
+		GPIO.softPwmCreate(self.ena, 0, 100)
+		GPIO.softPwmCreate(self.enb, 0, 100)
+
+	# Turn on PWM
+	def turn_on(self, duty=100):
+		GPIO.softPwmWrite(self.ena, duty)
+		GPIO.softPwmWrite(self.enb, duty)
+
 
 	# Begin defining directions
 	def backward(self):
-		# Set IN2 & IN4 low FIRST
-		GPIO.output((self.in2, self.in4), GPIO.LOW)
-		# Now set IN1 & IN3 high
-		GPIO.output((self.in1, self.in3), GPIO.HIGH)
+		
+		# Set the gates
+		GPIO.digitalWrite(self.in1, GPIO.LOW)
+		GPIO.digitalWrite(self.in3, GPIO.HIGH)
+		# Set PWM signals
+		self.turn_on()
 
 	def forward(self):
-		# Set IN1 & IN3 low
-		GPIO.output((self.in1, self.in3), GPIO.LOW)
-		# Now set IN2 & IN4 high
-		GPIO.output((self.in2, self.in4), GPIO.HIGH)
+		# Set the gates
+		GPIO.digitalWrite(self.in1, GPIO.HIGH)
+		GPIO.digitalWrite(self.in3, GPIO.LOW)
+		# Turn on motor
+		self.turn_on()
 
 	def left(self):
-		# Set IN1 & IN4 low first
-		GPIO.output((self.in1, self.in4), GPIO.LOW)
-		# Now set IN2 & IN3 high
-		GPIO.output((self.in2, self.in3), GPIO.HIGH)
+		# Set the gates
+		GPIO.digitalWrite(self.in1, GPIO.LOW)
+		GPIO.digitalWrite(self.in3, GPIO.LOW)
+		# Turn on motor
+		self.turn_on()
 
 	def right(self):
-		# SET IN2 & IN3 low first
-		GPIO.output((self.in2, self.in3), GPIO.LOW)
-		# Now set IN1 & IN4 high
-		GPIO.output((self.in1, self.in4), GPIO.HIGH)
+		# Set the gates
+		GPIO.digitalWrite(self.in1, GPIO.HIGH)
+		GPIO.digitalWrite(self.in3, GPIO.HIGH)
+		# Turn on motor
+		self.turn_on()
 
 	def stop(self):
 		# Set all inputs to low
-		GPIO.output((self.in1, self.in2, self.in3, self.in4), GPIO.LOW)
+		GPIO.softPwmWrite(self.ena, 0)
+		GPIO.softPwmWrite(self.enb, 0)
 
 
 	def runserver(self, callback, timeout=1):
@@ -80,11 +108,8 @@ class WifiCar:
 				else:
 					callback(self.serversocket, addr, content.strip())
 
-
-
-
-pins = (11, 12, 13, 15, 16, 18)
-car = WifiCar(pins)
+# pins are ENA, ENB, IN1, IN3
+car = WifiCar((ENA, ENB, IN1, IN3))
 FLAG = True
 
 # define our socket callback
@@ -123,25 +148,4 @@ while FLAG:
 	car.runserver(udp_socket_callback)
 
 
-GPIO.cleanup()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+clean()

6cf7d00c1c8b03cfb5a06d301cadb699a9023884
Isaiah <60484669+IsaiahST2020@users.noreply.github.com>
2022-03-31 16:14:21 -0400

    Edited broadcasting

--- CarPlatform/web/src/runServer.py
+++ CarPlatform/web/src/runServer.py
@@ -12,10 +12,6 @@ app.config.from_pyfile('config.py')
 ic2_bus = my_i2c.make_bus()
 my_i2c.motor_slow(ic2_bus)
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP) 
-sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-sock.bind(("127.0.0.1",31337))
-
 # Begin defining directions
 def backward():
     my_i2c.backward(ic2_bus)
@@ -33,53 +29,59 @@ def stop():
     my_i2c.stop(ic2_bus)
 
 def broadcast(msg):
-    global sock
-    sock.sendto(msg, ("255.255.255.255", 2022))
-    print("Sending message to scoring server")
+    b_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    b_sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    server_address = ('255.255.255.255', 2022)
+    msg = "[Broadcast] " + msg
+    b_msg = bytes(msg, 'ascii')
+    b_sock.sendto(b_msg, server_address)
 
 #-------------------------------------------------#
 # Objective 1 Storage
 #-------------------------------------------------#
 @app.route('/')
 def index():
+    broadcast("New Client Connected")
+    print("New client is now connecting")
     return render_template('index.html')
-    
+
 @app.route('/movement/', methods=['POST'])
 def move_input():
     content = request.json
-    
+
     if content['zoom'] == "forward":
         forward()
         print("User requested forward movement...")
-        broadcast("Car is moving forward!")
+        broadcast("Car is moving forward")
         return "Forward movement request successful!"
-        
+
     elif content['zoom'] == "left":
         left()
         print("User requested left movement...")
-        broadcast("Car is moving left!")
+        broadcast("Car is moving left")
         return "Left movement request successful!"
-        
+
     elif content['zoom'] == "right":
         right()
         print("User requested right movement...")
-        broadcast("Car is moving right!")
+        broadcast("Car is moving right")
         return "Right movement request successful!"
-        
+
     elif content['zoom'] == "backward":
         backward()
         print("User requested backward movement...")
-        broadcast("Car is moving backward!")
+        broadcast("Car is moving back")
         return "Backward movement request successful!"
-        
+
     elif content['zoom'] == "stop":
         stop()
         print("User requested to stop movement...")
-        broadcast("Car is stopping!")
+        broadcast("Car is stopping")
         return "Stopping movement!"
-    
-    else:    
+
+    else:
+        broadcast("Strange traffic is reaching the car!")
         return "Bad movement request!"
-       
+
 if __name__ == '__main__':
     app.run(debug=True, port=80, host='0.0.0.0')
